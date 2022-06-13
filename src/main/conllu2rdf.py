b488d5f00e865509b598c10211af2073c037a59d
Gennaro Nolano <nolanogenn@gmail.com>
2022-06-13 14:35:33 +0200

    script completed

--- src/main/conllu2rdf.py
+++ src/main/conllu2rdf.py
@@ -4,6 +4,7 @@ import pandas as pd
 from rdflib import Graph, Literal, URIRef
 from rdflib.namespace import Namespace, NamespaceManager
 from rdflib.namespace import RDF
+from progress.bar import Bar
 
 graph = Graph()
 
@@ -11,7 +12,7 @@ graph = Graph()
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--input', help='The input TSV file')
-parser.add_argument('--output', help='The output RDF file')
+parser.add_argument('--output', help='The output RDF file', default='example.ttl')
 parser.add_argument('--columns', help='The columns defined in the TSV file (list of columns divided by a space)')
 parser.add_argument('--uri', help='The basic uri of your resource')
 
@@ -24,7 +25,7 @@ sentences = []
 sentence = []
 for line in input_tsv:
     if line != '\n':
-        sentence.append(line.split('\t'))
+        sentence.append([x.replace('\n', '') for x in line.split('\t')])
     else:
         sentences.append(sentence)
         sentence = []
@@ -47,6 +48,7 @@ for prefix in prefixes:
 nif_sentence = Namespace(prefixes['nif']).Sentence 
 nif_word = Namespace(prefixes['nif']).Word
 nif_next_word = Namespace(prefixes['nif']).nextWord
+nif_has_word = Namespace(prefixes['nif']).hasWord
 
 col_classes = {
         "WORD":Namespace(prefixes['conll']).WORD,
@@ -58,31 +60,38 @@ col_classes = {
         "HEAD":Namespace(prefixes['conll']).HEAD,
         "EDGE":Namespace(prefixes['conll']).EDGE,
         "DEPS":Namespace(prefixes['conll']).DEPS,
+        "LABEL":Namespace(prefixes['conll']).LABEL,
        }
 
-for sentence_enum, sentence in enumerate(sentences):
-    id_sentence = URIRef(f"{prefixes['']}{sentence_enum}_0")
+with Bar("Processing", max=len(sentences)) as bar:
+    for sentence_enum, sentence in enumerate(sentences):
+        id_sentence = URIRef(f"{prefixes['']}{sentence_enum}_0")
 
-    uri_sentence = f"{id_sentence}"
-    
-    graph.add((id_sentence, RDF.type, nif_sentence))
-    for word in sentence:
-        id_word = URIRef(f"{prefixes['']}s{sentence_enum}_{word[0]}")
-        graph.add((id_word, RDF.type, nif_word))
+        uri_sentence = f"{id_sentence}"
         
-        for index_col, col in enumerate(colnames):
-            if word[index_col] != '_' and col != 'ID':
-                rel = col_classes[col]
-                if col != "HEAD":
-                    graph.add((id_word, rel, Literal(word[index_col])))
-                else:
-                    id_head = URIRef(f"{prefixes['']}s{sentence_enum}_{word[index_col]}")
-                    graph.add((id_word, rel, id_head))
+        graph.add((id_sentence, RDF.type, nif_sentence))
+        for index_word, word in enumerate(sentence):
+            id_word = URIRef(f"{prefixes['']}s{sentence_enum}_{word[0]}")
+            graph.add((id_word, RDF.type, nif_word))
+            graph.add((id_sentence, nif_has_word, id_word))
+            
 
-    if sentence_enum == 5:
-        break
+            if index_word+1  < len(sentence):
+                next_word_id = URIRef(f"{prefixes['']}s{sentence_enum}_{sentence[index_word+1][0]}")
+                graph.add((id_word, Namespace(prefixes["nif"]).nextWord, next_word_id))
 
-print(graph.serialize())
+            for index_col, col in enumerate(colnames):
+                if word[index_col] != '_' and col != 'ID':
+                    rel = col_classes[col]
+                    if col != "HEAD":
+                        graph.add((id_word, rel, Literal(word[index_col])))
+                    else:
+                        id_head = URIRef(f"{prefixes['']}s{sentence_enum}_{word[index_col]}")
+                        graph.add((id_word, rel, id_head))
+        bar.next()
 
+outputfile = args.output
+print("Serializing the output file...")
+graph.serialize(destination=outputfile)
 
 
